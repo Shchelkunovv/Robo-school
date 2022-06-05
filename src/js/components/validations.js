58dc308082274b56270b547a9262e0214ae9ff85
Evgeniy Shchelkunov <shchelkunov.zhen9@gmail.com>
2022-06-05 21:46:33 +0700

    commit

---
+++ src/js/components/validations.js
@@ -0,0 +1,49 @@
+import { validateForms } from '../functions/validate-forms';
+
+const rules = [
+   {
+      ruleSelector: '.input-name',
+      rules: [
+         {
+            rule: 'required',
+            value: true,
+            errorMessage: 'Заполните имя!'
+         }
+      ]
+   },
+   {
+      ruleSelector: '.input-tel',
+      tel: true,
+      telError: 'Введите корректный телефон',
+      rules: [
+         {
+            rule: 'required',
+            value: true,
+            errorMessage: 'Заполните телефон!'
+         }
+      ]
+   },
+   {
+      ruleSelector: '.input-email',
+      tel: true,
+      telError: 'Введите e-mail',
+      rules: [
+         {
+            rule: 'required',
+            value: true,
+            errorMessage: 'Введите e-mail!'
+         },
+         {
+            rule: 'email',
+            value: true,
+            errorMessage: 'Введите корректный e-mail!'
+         }
+      ]
+   },
+];
+
+const afterForm = () => {
+   console.log('Произошла отправка, тут можно писать любые действия');
+};
+
+validateForms('.order__form', rules, afterForm);
